1458a781746c5003d8879c94d704a8f3bcbc8d29
slemeshaev <lemeshaevs@gmail.com>
2021-02-09 21:19:56 +0300

    Missed calls

--- MissedCalls/MissedCalls/Helpers/Extensions.swift
+++ MissedCalls/MissedCalls/Helpers/Extensions.swift
@@ -11,6 +11,28 @@ public struct AnchoredConstraints {
     public var top, leading, bottom, trailing, width, height: NSLayoutConstraint?
 }
 
+extension UIViewController {
+    
+    func configureNavigationBar(withTitle title: String, prefersLargeTitles: Bool) {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+        navigationController?.navigationBar.prefersLargeTitles = prefersLargeTitles
+        navigationItem.title = title
+        navigationController?.navigationBar.tintColor = .black
+        navigationController?.navigationBar.isTranslucent = true
+        
+        navigationController?.navigationBar.overrideUserInterfaceStyle = .light
+    }
+    
+}
+
 extension UIView {
     
     func anchor(top: NSLayoutYAxisAnchor? = nil,
