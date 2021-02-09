e6104bb1705dcf1c2050c410f1e5edfa674ebbfd
slemeshaev <lemeshaevs@gmail.com>
2021-02-09 20:11:21 +0300

    MissedCallsController

--- MissedCalls/MissedCalls/Controller/MissedCallsController.swift
+++ MissedCalls/MissedCalls/Controller/MissedCallsController.swift
@@ -5,4 +5,23 @@
 //  Created by Станислав Лемешаев on 09.02.2021.
 //
 
-import Foundation
+import UIKit
+
+class MissedCallsController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        cofigureUI()
+    }
+    
+    // MARK: - Helpers
+    
+    func cofigureUI() {
+        view.backgroundColor = .blue
+    }
+    
+}
