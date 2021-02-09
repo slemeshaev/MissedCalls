85682fbbd996e9760a5d0abd09b81a6332c4caa7
slemeshaev <lemeshaevs@gmail.com>
2021-02-09 22:26:21 +0300

    ui elements

--- MissedCalls/MissedCalls/View/Cell/MissedCallsCell.swift
+++ MissedCalls/MissedCalls/View/Cell/MissedCallsCell.swift
@@ -11,6 +11,42 @@ class MissedCallsCell: UITableViewCell {
     
     // MARK: - Properties
     
+    private let callImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = #imageLiteral(resourceName: "callInboundMissed")
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+    
+    private let talkTimeLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.italicSystemFont(ofSize: 13)
+        label.text = "00:13"
+        return label
+    }()
+    
+    private let nameOfContactLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 17)
+        label.text = "Name of contact"
+        return label
+    }()
+    
+    private let numberOfContactLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17)
+        label.text = "+1 800 123-4567"
+        return label
+    }()
+    
+    let timestampLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 13)
+        label.textColor = .darkGray
+        label.text = "10:21 AM"
+        return label
+    }()
     
     
     // MARK: - Lifecycle
@@ -22,5 +58,7 @@ class MissedCallsCell: UITableViewCell {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    
     
 }
