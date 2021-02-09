47341d46fb5811b27f504d1867817113bab8d836
slemeshaev <lemeshaevs@gmail.com>
2021-02-09 19:13:34 +0300

    files for projects

---
+++ MissedCalls/MissedCalls/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MissedCalls
+//
+//  Created by Станислав Лемешаев on 09.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
