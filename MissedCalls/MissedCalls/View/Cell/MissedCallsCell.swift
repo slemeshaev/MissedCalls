088fa62617aebd08d5e7fee41ea38377fe269d19
slemeshaev <lemeshaevs@gmail.com>
2021-02-09 21:32:12 +0300

    MissedCallsCell

--- MissedCalls/MissedCalls/View/Cell/MissedCallsCell.swift
+++ MissedCalls/MissedCalls/View/Cell/MissedCallsCell.swift
@@ -5,4 +5,22 @@
 //  Created by Станислав Лемешаев on 09.02.2021.
 //
 
-import Foundation
+import UIKit
+
+class MissedCallsCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    
+    
+    // MARK: - Lifecycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
