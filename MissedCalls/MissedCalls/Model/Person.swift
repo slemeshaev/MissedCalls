36553f91b4b98687344af5316d0b7f3321bf0ab6
slemeshaev <lemeshaevs@gmail.com>
2021-02-10 00:16:11 +0300

    person model

--- MissedCalls/MissedCalls/Model/Person.swift
+++ MissedCalls/MissedCalls/Model/Person.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct Person {
+    let id: String
+    let client: Client
+    let businessNumber: BusinessNumber
+    let duration: String
+}
+
+struct Client {
+    let address: String
+    let Name: String
+}
+
+struct BusinessNumber {
+    let number: String
+    let label: String
+}
