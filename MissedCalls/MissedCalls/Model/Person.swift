732b8d8c093a84723b8e554313c1e17d1077812b
slemeshaev <lemeshaevs@gmail.com>
2021-02-10 18:37:34 +0300

    json string any

--- MissedCalls/MissedCalls/Model/Person.swift
+++ MissedCalls/MissedCalls/Model/Person.swift
@@ -12,14 +12,34 @@ struct Person {
     let client: Client
     let businessNumber: BusinessNumber
     let duration: String
+    
+    init(json: [String: Any]) {
+        self.id = json["id"] as? String ?? ""
+        let clientJson = json["client"] as! [String: Any]
+        self.client = Client(json: clientJson)
+        let businessNumberJson = json["businessNumber"] as! [String: Any]
+        self.businessNumber = BusinessNumber(json: businessNumberJson)
+        self.duration = json["duration"] as? String ?? ""
+    }
+    
 }
 
 struct Client {
     let address: String
     let Name: String
+    
+    init(json: [String: Any]) {
+        self.address = json["address"] as? String ?? ""
+        self.Name = json["Name"] as? String ?? ""
+    }
 }
 
 struct BusinessNumber {
     let number: String
     let label: String
+    
+    init(json: [String: Any]) {
+        self.number = json["number"] as? String ?? ""
+        self.label = json["label"] as? String ?? ""
+    }
 }
